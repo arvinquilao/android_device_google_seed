29f903d68e2b6a8caa2ebb31c7ddbcc2b846011f
Arvin Quilao <arquilao@gmail.com>
2018-01-18 17:21:28 +0800

    DNM: Permissive selinux

--- board/kernel.mk
+++ board/kernel.mk
@@ -4,3 +4,4 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 TARGET_KERNEL_SOURCE := kernel/google/seed
 TARGET_KERNEL_CONFIG := lineageos_seed_defconfig
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
